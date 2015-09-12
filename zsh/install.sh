8879b9e6aea3fb0bc46d570e2c508fab784dcd1c
Josh Kaufman <me@awesomejosh.com>
2015-09-12 03:34:03 -0400

    add custom overrides for oh-my-zsh && add boilerplate for ruby

--- zsh/install.sh
+++ zsh/install.sh
@@ -3,6 +3,7 @@
 # oh-my-zsh
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
   sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+  cd /tmp && mkdir dotfile && cd ./dotfile && git clone https://github.com/JoshKaufman/oh-my-zsh-custom-config.git && mv ./oh-my-zsh-custom-config $HOME/.oh-my-zsh/custom
 fi
 
 exit 0
