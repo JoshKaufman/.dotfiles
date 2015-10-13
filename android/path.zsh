297658c75961c55f99ad25f86b6b0e63f30c15d2
Josh Kaufman <me@awesomejosh.com>
2015-10-13 02:41:56 -0400

    add jdk path, fixup MANPATH

---
+++ android/path.zsh
@@ -0,0 +1,5 @@
+# JDK home for android studio (jdk installed via brew cask)
+export STUDIO_JDK="/Library/Java/JavaVirtualMachines/jdk1.8.0_45.jdk"
+
+
+export MANPATH="$MANPATH:/Library/Java/JavaVirtualMachines/jdk1.8.0_45.jdk/Contents/Home/man"
