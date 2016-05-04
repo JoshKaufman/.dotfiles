6319717fae56feac8c95d1da730f103b95ccce72
Josh Kaufman <me@awesomejosh.com>
2016-05-04 00:54:10 -0400

    add handy git aliases

--- git/alias.export.zsh
+++ git/alias.export.zsh
@@ -5,3 +5,44 @@ alias gitdb="git diff master.."
 
 # semi related
 alias sdiff="diff --side-by-side --suppress-common-lines "
+
+gupdate() {
+    echo "updating all repos in $(pwd)";
+    dirs=(./*/);
+    if [ ${#dirs[@]} -gt 0 ]; then
+        find ./* ! -name '.git' -type d -maxdepth 0 -print0 | while IFS= read -r -d '' folder; do
+            echo "entering ${folder}";
+            cd "$folder";
+            if [ -d "./.git" ]; then
+                echo "checking for updates";
+                git fetch --all;
+                git pull;
+                echo "pull was success leaving ${folder}";
+                cd ..;
+            else
+                echo "${folder} is not a git directory.. checking children";
+                gupdate;
+                cd ..;
+            fi
+        done
+        echo "$(pwd) is now up to date with all remotes";
+    else
+        echo "no subdirectories found.";
+    fi
+}
+
+gupdate2() {
+    echo "updateing all repos in $(pwd)";
+    dirs = find . -name '.git' -type d -exec '{}' +
+    if [ ${#dirs[@]} -gt 0 ]; then
+        find . -name '.git' -type d -print0 | while IFS= read -r -d '' gitfolder; do
+            cd "$folder";
+            cd ..;
+            git fetch;
+            git pull;
+            echo "updated $(pwd)";
+        done
+    else
+        echo "no git repos found!!"
+    fi
+}
